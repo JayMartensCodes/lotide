b2919b1e5172cb848a2bf28437850f5d106c1c3d
Jay Martens <martensjay93@gmail.com>
2020-07-21 13:35:27 -0400

    Created a function to assert whether or not a function is equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = (firstArray, secondArray) => {
+  if (Array.isArray(firstArray) && Array.isArray(secondArray) && firstArray.length === secondArray.length) {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (firstArray, secondArray) => {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✔✔✔ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,2,3], [1,2,3,4]);
